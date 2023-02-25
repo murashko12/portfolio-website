a082bd1d0831a23b89723f8ee1c9c1b68ef767bd
murashko12 <pert@ocumare.ru>
2023-02-25 12:34:50 +0300

    footer is finish

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -11,23 +11,19 @@ const NavBar = () => {
         {
             id: 1,
             link: 'home'
-        },
-        {
+        },{
             id: 2,
             link: 'about'
-        },
-        {
+        },{
             id: 3,
             link: 'portfolio'
-        },
-        {
+        },{
             id: 4,
             link: 'skills'
-        },
-        {
+        },{
             id: 5,
             link: 'contact'
-        },
+        }
     ]
 
     return (
