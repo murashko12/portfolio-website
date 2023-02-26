28546b2835f8cee0224196ce82b6cf4845cf87a9
murashko12 <pert@ocumare.ru>
2023-02-26 22:58:15 +0300

    SocialLinks is ready

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './SocialLinks.css'
 import { FaTelegramPlane,FaGithub } from "react-icons/fa";
 import { SlSocialVkontakte } from "react-icons/sl";
 import { GoMail } from "react-icons/go";
-
+import {AiOutlinePlus} from 'react-icons/ai';
 const SocialLinks = () => {
 
     
@@ -47,21 +47,62 @@ const SocialLinks = () => {
     ]
     
 
+    const [isOpen, setIsOpen] = useState(false)
 
-  return (
-    <div className='SocialLinks'>
-        <ul>
-
-            {links.map(({id,child,href}) => {return(
-                    <li key={id}>
-                        <a href={href} target="_blank" rel="noreferrer">
-                            {child}
-                        </a>
-                    </li>
-                )}     
-            )}
+    return (
+        <div className='SocialLinks'>
+            <ul>
+                {links.map(({id,child,href}) => {
+                    return(
+                        <li key={id}>
+                            <a href={href} target="_blank" rel="noreferrer">
+                                {child}
+                            </a>
+                        </li>
+                    )}     
+                )}
 
         </ul>
+
+        <div className={`fab ${isOpen ? "open" : ""}`}>
+            <button onClick={() => setIsOpen(!isOpen)}>
+                <>
+                    <AiOutlinePlus size={30} />
+                </>
+            </button>
+
+            <div className="menu">
+
+                <button>
+                    <a href="https://github.com/murashko12" target="_blank" rel="noreferrer">
+                        <> <FaGithub size={30} /></>
+                    </a>
+                    {/* <span>GitHub</span> */}
+                </button>
+                <button>
+                    <a href="https://t.me/Peter_Muraschko" target="_blank" rel="noreferrer">
+                        <><FaTelegramPlane size={30} /></>
+                    </a>
+                    {/* <span>GitHub</span> */}
+                </button>
+                <button>
+                    <a href="https://vk.com/id222310051" target="_blank" rel="noreferrer">
+                        <> <SlSocialVkontakte size={30} /></>
+                    </a>
+        
+                    {/* <span>GitHub</span> */}
+                </button>
+                <button>
+                    <a href="mailto:petr@ocumare.ru" target="_blank" rel="noreferrer">
+                        <> <GoMail size={30} /></>
+                    </a>
+                    
+                    {/* <span>GitHub</span> */}
+                </button>
+                
+            </div>
+        </div>
+
     </div>
   )
 }
