c35ff908365c3e5f504263cbf6f1f547f577d43f
murashko12 <pert@ocumare.ru>
2023-02-26 23:09:59 +0300

    fix SocialLinks

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -99,7 +99,6 @@ const SocialLinks = () => {
                     
                     {/* <span>GitHub</span> */}
                 </button>
-                
             </div>
         </div>
 
