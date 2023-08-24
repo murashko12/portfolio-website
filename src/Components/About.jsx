3abc0a78a517275d4795719869cbc68c7cacac80
murashko12 <pert@ocumare.ru>
2023-08-24 15:08:45 +0300

    fix mistakes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
                 <h1>About</h1>   
             </div>
             <p>
-                I’ve been studying in the third year of the bachelor's degree program at the NRU MEI with the degree in 09.03.01 "Computer Science and Computer Engineering". 
+                I’ve been studying in the third year of the bachelor's degree program at the NRU MPEI with the degree in 09.03.01 "Computer Science and Computer Engineering". 
                 I’ve taken courses in Web development from "<a href="https://thmoon.ru/" target="_blank">thmoon</a>". 
-                I’ve worked with "Logistics Engineering Center". I'm still improving my React skills. My English level is B2 "Upper-Intermediate". 
+                I’ve worked with "Logistics Engineering Center". I'm still improving my React skills. My English level is C1 "Advanced". 
                 Personal qualities: purposeful, able to work in a team, pragmatic, stress-resistant and creative.
             </p>
         </div>
